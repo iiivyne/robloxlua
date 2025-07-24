599f0ca19d58f55758df2ff35359df03287b3bf5
[namespace] <83887777+iiivyne@users.noreply.github.com>
2025-07-24 15:53:18 -0700

    Create gameloader.lua

---
+++ gameloader.lua
@@ -0,0 +1,25 @@
+local placeScripts = {
+    [126509999114328] = function()
+        -- script content
+        print("game: 99 nights in the forest")
+        -- require or execute your place-specific code here
+    end,
+    [987654321] = function()
+        -- script content
+        print("game: survival odyssey")
+        -- require or execute your place-specific code here
+    end,
+    [125009265613167] = function()
+        -- script content
+        print("game: ink game")
+        -- require or execute your place-specific code here
+    end,
+    -- more place ids here
+}
+
+local currentScript = placeScripts[game.PlaceId]
+if currentScript then
+    currentScript()
+else
+    game:GetService("Players").LocalPlayer:Kick("Unsupported game. PlaceId: " .. tostring(game.PlaceId))
+end
